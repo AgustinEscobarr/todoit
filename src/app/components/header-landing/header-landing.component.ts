f54c8147a7b7061fa84c00ae8a423829ad98a81b
AgustinEscobarr <agustinescobaring@gmail.com>
2021-11-23 02:03:27 -0300

    se empiezan a configurar los estilos

--- src/app/components/header-landing/header-landing.component.ts
+++ src/app/components/header-landing/header-landing.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header-landing',
   templateUrl: './header-landing.component.html',
-  styleUrls: ['./header-landing.component.sass']
+  styleUrls: ['./header-landing.component.scss']
 })
 export class HeaderLandingComponent implements OnInit {
 
